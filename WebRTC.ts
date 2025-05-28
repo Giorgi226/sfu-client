9cd6111f627e7aace2e36a163d8972ca77fc9a6e
Giorgi226 <giorgi.khakhutashvili26@gmail.com>
2025-05-28 16:36:44 +0400

    change consoling ice candidates

--- WebRTC.ts
+++ WebRTC.ts
@@ -101,6 +101,9 @@ export class WebRTC {
         }else {
             const transport = this.device?.createSendTransport(data.parameters)
             console.log('iceee candidateesssss: ', data.parameters.iceCandidates)
+            console.log('iceee candidateesssss: ', data.parameters)
+            console.log('iceee candidateesssss: ', data)
+            console.log('iceee candidateesssss: ')
             this.outTransport = transport!
         transport?.on('connect', async ({dtlsParameters}, callback, errback) => {
         
