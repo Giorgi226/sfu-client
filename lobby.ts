0ed010b4587b3a8e83c398711e57c80b5e39bcc0
Giorgi226 <giorgi.khakhutashvili26@gmail.com>
2025-05-28 10:12:45 +0400

    change lobby.ts

--- lobby.ts
+++ lobby.ts
@@ -1,11 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {  
 const form = document.getElementById('join-form') as HTMLFormElement
+ if (!form) {
+    console.error('Join form not found!');
+    return;
+  }
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     
     const invitecode = (e.target as any).room.value
     const newUrl = `${window.location.origin}/index.html?room=${encodeURIComponent(invitecode)}`;
+    console.log('Redirecting to:', newUrl);
     window.location.replace(newUrl);
 })
 });
